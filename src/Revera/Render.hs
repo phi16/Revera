f5a5ee993a28fcc9b40f591f5dd575ec01e93f17
phi16 <phi16.256@gmail.com>
2016-03-18 00:07:51 +0900

    Add : Swing logo

--- src/Revera/Render.hs
+++ src/Revera/Render.hs
@@ -20,12 +20,15 @@ simplify :: Picture -> Picture
 simplify = id
 
 render w = return $ onRender $ do
-  draw $ color (dark $ dark blue) $ translate (-187) (-100) $ zoom 75 $ polygon [(-1,-1),(1,-1),(-1,1)]
-  draw $ color (dark $ dark blue) $ translate (188) (-100) $ zoom 75 $ polygon [(1,-1),(1,1),(-1,1)]
+  let
+    sc = (*5) $ sin $ (*2) $ w^.opTime
+    cc = (*5) $ cos $ (*2) $ w^.opTime
+  draw $ color (greyN 0.2 * violet) $ translate (-187) (-100+sc) $ zoom 75 $ polygon [(-1,-1),(1,-1),(-1,1)]
+  draw $ color (greyN 0.2 * blue) $ translate (188) (-100+sc) $ zoom 75 $ polygon [(1,-1),(1,1),(-1,1)]
   draw $ color white $ translate 0 (-100) $ pictures [
-    zoom 75 $ centerString "R A",
-    zoom 25 $ centerString "eVEr"]
-  draw $ color white $ translate 0 30 $ zoom 10.6 $ centerString "PREss entER to staRt"
+    translate 0 sc $ zoom 75 $ centerString "R A",
+    translate 0 (sc/2) $ zoom 25 $ centerString "eVEr"]
+  draw $ color white $ translate 0 (30+cc/2) $ zoom 10.6 $ centerString "PREss entER to staRt"
   draw $ color white $ translate (-257) 180 $ zoom 7 $ fontString "find the way to "
   draw $ color white $ translate (-257) 200 $ zoom 7 $ fontString "get out of fRame"
   draw $ color white $ translate 40 120 $ zoom 10 $ fontString $ show $ w^.opTime
